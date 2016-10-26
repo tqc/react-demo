c73e2dd52d28be70eeb11f72e1a17318366f3710
Tom Clarkson <tom@orangeguava.com>
2016-10-26 11:33:31 +1100

    add github link

--- client/ui/about.js
+++ client/ui/about.js
@@ -11,6 +11,13 @@ class StartPage extends React.Component {
                 <div className="content">
                     <p>This small demo app was developed by <a href="http://tqclarkson.com">Tom Clarkson</a> to fix a shortage of shareable React code in his <a href="http://tqclarkson.com/projects">portfolio</a> </p>
                 </div>
+
+                <div className="content">
+                    <p>
+                        See the code at <a href="https://github.com/tqc/react-demo">https://github.com/tqc/react-demo</a>
+                    </p>
+                </div>
+
                 <div className="content">
                 <p>Icons from <a href="https://linearicons.com/">https://linearicons.com/</a></p>
                 </div>
